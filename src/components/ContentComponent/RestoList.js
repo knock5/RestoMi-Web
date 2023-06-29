bf0915dfc7c065c5f16326d695e753c32a1ff020
knock5 <miftachulhd5@gmail.com>
2023-06-29 07:04:04 +0700

    configuration api

--- src/components/ContentComponent/RestoList.js
+++ src/components/ContentComponent/RestoList.js
@@ -1,4 +1,5 @@
 import RestodbSource from '../../data/restodb-source';
+import CONFIG from '../../globals/config';
 
 class RestoList extends HTMLElement {
   connectedCallback() {
@@ -13,7 +14,7 @@ class RestoList extends HTMLElement {
       this.innerHTML += `
         <div class="card">
             <span class="card-city">${res.city}</span>
-            <img src="${res.pictureId}" alt="image ${res.name} restaurant" class="card-image">
+            <img src="${CONFIG.BASE_IMAGE_URL_SMALL + res.pictureId}" alt="image ${res.name} restaurant" class="card-image">
             <span class="card-rating">Rating : ${res.rating}</span>
             <h4 class="card-title">${res.name}</h4>
             <p class="card-desc">${res.description}</p>
